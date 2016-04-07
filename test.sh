289abed55456b20b0d352f168187d9477abccff8
bigbes <bigbes@gmail.com>
2016-04-07 18:12:03 +0300

    Only one must left

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y tarantool tarantool-dev --force-yes
 pip install --user python-daemon PyYAML six==1.9.0
 TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
 make internalso libmemcached
-make test-memcached test-memcached-capable
+make test-memcached

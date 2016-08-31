f66999fa88b76b3bfb41a2c000377d2097e12630
bigbes <bigbes@gmail.com>
2016-08-31 14:31:20 +0300

    Add necessary libs for python + add MP_SOURCE

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
-sudo apt-get install -y tarantool tarantool-dev libsasl2-dev sasl2-bin --force-yes
-pip install --user python-daemon PyYAML six==1.9.0
+sudo apt-get install -y tarantool tarantool-dev --force-yes
+pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent
 TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
 make internalso libmemcached
 make test-memcached

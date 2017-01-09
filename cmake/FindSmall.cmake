ba1874618a1b169711751a89dc8e14e73ea07c17
Konstantin Nazarov <mail@racktear.com>
2017-01-09 16:19:17 +0300

    Fix usage of third_party from small
    small library has changed the location of some headers
    to third_party/, thus we need to add that subdir to includes,
    otherwise memcached will fail to compile.

--- cmake/FindSmall.cmake
+++ cmake/FindSmall.cmake
@@ -12,7 +12,7 @@ find_library(SMALL_LIBRARY NAMES small)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(small
     REQUIRED_VARS SMALL_INCLUDE_DIR SMALL_LIBRARY)
-set(SMALL_INCLUDE_DIRS ${SMALL_INCLUDE_DIR})
+set(SMALL_INCLUDE_DIRS ${SMALL_INCLUDE_DIR} ${SMALL_INCLUDE_DIR}/small/third_party)
 set(SMALL_LIBRARIES ${SMALL_LIBRARY})
 mark_as_advanced(SMALL_INCLUDE_DIR SMALL_INCLUDE_DIRS
                  SMALL_LIBRARY SMALL_LIBRARIES)

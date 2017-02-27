f0b4a5760dbeb7aeb1e512d766d3fcc417ed0bdc
Roman Tsisyk <roman@tsisyk.com>
2017-02-27 15:25:18 +0300

    Travis CI: fix python-gevent version

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
 sudo apt-get install -y tarantool tarantool-dev libevent-dev libsasl2-dev --force-yes
-pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent
+pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent==1.1.2
 TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
 make internalso libmemcached
 make test-memcached

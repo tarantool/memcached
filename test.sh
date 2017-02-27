29fbcfdfc26da1786c2fbbba8fb62878d5bb1500
Roman Tsisyk <roman@tsisyk.com>
2017-02-27 14:31:07 +0300

    Travis CI: update to the latest version of .travis.yml
    * Switch to PackPack for packages
    * Use `packagecloud` deployment instead of travis.sh script
    * Enable email notifications
    * Add Fedora 25, Ubuntu Yakkety
    * Remove Ubuntu Wily (EOL)

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
 sudo apt-get install -y tarantool tarantool-dev --force-yes
 pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent

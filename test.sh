58fa1e946abe114336f92976a65549530dce84cc
Roman Tsisyk <roman@tsisyk.com>
2017-02-27 14:59:23 +0300

    Travis CI: add missing dependencies
    Fixes #36

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
-sudo apt-get install -y tarantool tarantool-dev --force-yes
+sudo apt-get install -y tarantool tarantool-dev libevent-dev libsasl2-dev --force-yes
 pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent
 TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
 make internalso libmemcached

0b35a56c28872b1d22d64f58c43133673cb20e77
Alexander Turenko <alexander.turenko@tarantool.org>
2019-07-25 16:03:42 +0300

    travis-ci: update tarantool to 1.10 in a test job
    I have got the following error in Travis-CI for tarantool-1.6 repository:
    
     | The following packages have unmet dependencies:
     |  tarantool : Depends: tarantool-common (>= 1.6.9) but it is not going to be installed
     |              Conflicts: libtarantool-dev (< 1.6~) but 1.5.1.218.g1a69fd6-1ubuntu1 is to be installed
     | E: Unable to correct problems, you have held broken packages.
    
    Our 1.6 repository lack of libtarantool-dev (but have tarantool-dev) packages,
    don't sure why and whether it is expected. Anyway it worth to move to 1.10.
    
    Updated .tarantoolctl to reflect box.cfg option names changes between 1.6 and 1.10.
    
    Updated text/multiversioning.test.py result file, but to be honest I don't sure
    what is the reason of SELECT statistics change. At least those values are the
    same before and after the test, so I guess they are correct.
    
    Part of #49.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
-sudo apt-get install -y tarantool libtarantool-dev libevent-dev libsasl2-dev --force-yes
+curl -s https://packagecloud.io/install/repositories/tarantool/1_10/script.deb.sh | sudo bash
+sudo apt-get install -y tarantool tarantool-dev libevent-dev libsasl2-dev --force-yes
 pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent==1.1.2
 TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
 make internalso libmemcached

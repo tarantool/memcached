27c7ea0b6e0bb8eb9df18da047061b781c884a79
Artem Starshov <artemreyt@tarantool.org>
2020-10-28 09:42:27 +0000

    ci: add verify on different tarantool versions
    Now, Travis CI will run tests for memcached module
    using Tarantool versions 1.10, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7
    
    Added few jobs to .travis.yml for this feature.
    Updated way to enable tarantool repo to
    `curl -L https://tarantool.io/installer.sh | sudo VER=2.5 bash`
    in test.sh and made TARANTOOL_VERSION variable in test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,10 @@
 
 set -eu  # Strict shell, w/o print commands (set -x), w/o -o pipefail
 
-curl -s https://packagecloud.io/install/repositories/tarantool/1_10/script.deb.sh | sudo bash
-sudo apt-get install -y tarantool tarantool-dev libevent-dev libsasl2-dev --force-yes
+TARANTOOL_VERSION=${TARANTOOL_VERSION:-1.10}
+
+curl -sL https://tarantool.io/installer.sh | sudo VER=$TARANTOOL_VERSION bash
+sudo apt-get install -y libevent-dev libsasl2-dev --force-yes
 pip install --user python-daemon PyYAML six==1.9.0 msgpack-python gevent==1.1.2
 TARANTOOL_DIR=/usr/include cmake . -DCMAKE_BUILD_TYPE=Release
 

1d47fa881cf52837942c9dc450c36b2d6da8c13f
Sergey Bronnikov <sergeyb@tarantool.org>
2021-12-20 16:55:38 +0300

    third_party: replace libmemcached Git submodule with binary package
    libmemcached is a project that provides a library with implemenation of
    memcached protocol and a number of tools for working with memcached
    servers. memcached uses memcapable [1] that checks memcached server
    implemenation for conformance with memcached protocol. libmemcached
    available as binary packages on Ubuntu (memcapable is in
    libmemcached-tools, see [2]) and no reasons to keep libmemcached as a Git
    submodule and spend time on it's compilation and maintenance.
    
    1. http://docs.libmemcached.org/bin/memcapable.html
    2. https://packages.ubuntu.com/focal/libmemcached-tools
    
    Closes #90

--- test/capable/capable-text.test.py
+++ test/capable/capable-text.test.py
@@ -15,7 +15,7 @@ from internal.memcached_connection import STATUS, COMMANDS
 mc = MemcachedBinaryConnection("127.0.0.1", iproto.py_con.port)
 mc.flush()
 
-cmd = shlex.split('capable/memcapable -a -p %s -h %s -v' %
+cmd = shlex.split('memccapable -a -p %s -h %s -v' %
         (iproto.py_con.port, '127.0.0.1'))
 
 task = Popen(cmd, stdout=PIPE, stderr=STDOUT)

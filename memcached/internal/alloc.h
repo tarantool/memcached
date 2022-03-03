a82449c9ebd459880f4a49a5c34d4dcc52a832a1
Sergey Bronnikov <sergeyb@tarantool.org>
2022-03-03 11:49:32 +0300

    memcached: remove dead code

--- memcached/internal/alloc.h
+++ memcached/internal/alloc.h
@@ -1,22 +1,4 @@
 #ifndef   ALLOC_H_INCLUDED
 #define   ALLOC_H_INCLUDED
 
-void *
-box_persistent_malloc(size_t len, size_t *total);
-
-void *
-box_persistent_malloc0(size_t len, size_t *total);
-
-void
-box_persistent_free(void *ptr);
-
-void *
-box_persistent_realloc(void *ptr, size_t len, size_t *total);
-
-void *
-box_persistent_strdup(const void *str, size_t *total);
-
-void *
-box_persistent_strndup(const void *str, size_t len, size_t *total);
-
 #endif /* ALLOC_H_INCLUDED */

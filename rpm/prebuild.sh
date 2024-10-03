c5fd957c5db740ef679382ff1ca217f145ae7600
themilchenko <ivan.milchenko.92@mail.ru>
2024-10-03 09:52:25 +0300

    ci: add almalinux platform to packaging
    Almalinux 8, 9 needs to be supported and wasn't included to the platform
    list, but `packpack/packpack` supports it.
    
    After the patch this platform has been included.
    
    Closes #128

--- rpm/prebuild.sh
+++ rpm/prebuild.sh
@@ -2,4 +2,21 @@
 
 set -exu  # Strict shell (w/o -o pipefail)
 
-curl -LsSf https://www.tarantool.io/release/1.10/installer.sh | sudo bash
+os=""
+dist=""
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    os="$ID"
+    dist="$VERSION_ID"
+fi
+
+# The installer script is ending with unexpected error of unsupported OS for
+# "Almalinux 9". Here we fix this by overriding the cenos-release file so that
+# the enviroment settings work correctly.
+if [[ "$os" == "almalinux" && "$dist" == 9* ]]; then
+    sudo sh -c "echo 8 > /etc/centos-release"
+    curl -LsSf https://tarantool.io/release/3/installer.sh | sudo bash
+else
+    # We need to execute the 1.10 script due to it works for other platforms.
+    curl -LsSf https://tarantool.io/release/1.10/installer.sh | sudo bash
+fi
